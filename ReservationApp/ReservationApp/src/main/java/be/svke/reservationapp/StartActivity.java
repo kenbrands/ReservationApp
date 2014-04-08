30ac14249b5b4effadd707133506397db78af081
Jean Smits <jeansmits@iMac-van-Jean-5.local>
2014-04-08 11:37:36 +0200

    first commit

---
+++ ReservationApp/ReservationApp/src/main/java/be/svke/reservationapp/StartActivity.java
@@ -0,0 +1,22 @@
+package be.svke.reservationapp;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startrelative);
+
+
+        /*
+        EVEN TESTEN MET GITHUB BLA BLA BLA
+        BLA BLA BLA GITHUB BLA BLA BLA
+        BLA BLA BLA GITHUB BLA BLA BLA
+        BLA BLA BLA GITHUB BLA BLA BLA
+         */
+    }
+}
